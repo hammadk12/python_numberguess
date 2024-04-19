9643472ce585fbae7760cf76ba480a4a63a30254
hammadk12 <131798537+hammadk12@users.noreply.github.com>
2024-04-19 10:45:39 -0400

    initial commit - number guessing game

---
+++ index.py
@@ -0,0 +1,63 @@
+import random
+
+def main():
+    lower_bound = 1
+    upper_bound = 1000
+    max_attempts = 10
+    
+    while True:
+        print('1.Start Game')
+        print('2.Exit')
+
+        choice = input('Enter your choice: ')
+        
+        if choice == '1':
+            play_game(lower_bound, upper_bound, max_attempts)
+        elif choice == '2':
+            print('Exiting program.')
+            break
+
+# prompt user for input
+def get_guess(lower_bound, upper_bound):
+    while True:
+        try:
+            guess = int(input(f"Guess a number between {lower_bound} and {upper_bound}: "))  
+            if lower_bound <= guess <= upper_bound:
+                return guess
+            else:
+                print('Invalid input. Please enter a number within the specified range.')   
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")      
+
+# checks user guess
+def check_guess(guess, secret_number):
+    if guess == secret_number:
+        return 'Correct'
+    elif guess < secret_number:
+        return 'Too low'
+    else: 
+        return "Too high"
+    
+def play_game(lower_bound, upper_bound, max_attempts):
+    secret_number = random.randint(lower_bound, upper_bound)
+    attempts = 0
+    won = False
+    
+    while attempts < max_attempts:
+        attempts += 1
+        guess = get_guess(lower_bound, upper_bound)
+        result = check_guess(guess, secret_number)
+
+        if result == "Correct":
+            print(f"Congratulations! You guessed the secret number {secret_number} in {attempts} attempts.")
+            won = True
+            break
+        else:
+            print(f"{result}. Try again!")
+
+    if not won:
+        print(f"Sorry, you ran out of attempts! The secret number is {secret_number}.")
+
+if __name__ == "__main__":
+    print('Welcome to the Number Guessing Game!')
+    main()
